19d60689bf5ef57bd87bd9fe14c7e6e99e0916c4
Kevin Trotter <kevintrotter@google.com>
2018-05-11 16:11:15 -0700

    added /reset and /timestamp for remote reseting

--- app.js
+++ app.js
@@ -2,14 +2,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
 const app = express();
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+db.defaults({ timestamp: 0 }).write();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(express.static(path.join(__dirname, 'client')));
 
-app.get('*', (req, res) => {
+app.get('/timestamp', (req, res) => {
+    const timestamp = db.get('timestamp').value();
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify({timestamp:timestamp}));
+    res.end();
+});
+
+app.get('/reset', (req, res) => {
+    db.update('timestamp', n => n + 1).write();
+    const timestamp = db.get('timestamp').value();
+    res.write(`RESET: ${timestamp}`);
+    res.end();
+});
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/index.html'));
 });
 
